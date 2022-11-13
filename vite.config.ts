c589a1b3b29d8c4e85f3bcfb597f5fc161a8d26e
takehiro-1029 <masamasa.3332@gmail.com>
2022-11-13 21:01:02 +0900

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 1234,
     open: true,
+  },
+  resolve: {
+    alias: {
+      // https://qiita.com/koichi0909/items/648d1cfb415d16638509
+      "~/": `${__dirname}/src/`,
+    },
   },
   plugins: [react()]
 })
